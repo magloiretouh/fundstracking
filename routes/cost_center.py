1eeffe9149cc1b3dffd3a7bd9c336b98602d58a1
Magloire Touh <magloiretouh@gmail.com>
2023-08-31 11:03:33 +0000

    add id for coster center json

--- routes/cost_center.py
+++ routes/cost_center.py
@@ -8,4 +8,4 @@ cost_center_bp = Blueprint("cost_center", __name__, url_prefix="/cost_centers")
 @cost_center_bp.route('/<string:search_term>', methods=['GET'])
 def get_search_cost_centers(search_term):
     cost_centers = CostCenter.query.filter(CostCenter.libelle.ilike('%'+search_term+'%'))
-    return jsonify([{'cost_center': cost_center.libelle + " ("+ cost_center.code +")"} for cost_center in cost_centers])
+    return jsonify([{'cost_center': cost_center.libelle + " ("+ cost_center.code +")", 'id': cost_center.id} for cost_center in cost_centers])
---
+++ waitress_config.py
@@ -0,0 +1,1 @@
+waitress-serve --port 5050 app:app
